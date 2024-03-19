f41cc1d67c9940106bee5f8dc6fdb4b3bf1aecb9
vivek <vivekchaudhary160723@gmail.com>
2024-03-19 15:36:15 +0530

    leetcode 1

---
+++ leetcode1.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{}; 
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = sol.twoSum(nums, target);
+        System.out.println("The indices of the two numbers that add up to the target are: " + Arrays.toString(result));
+    }
+}
